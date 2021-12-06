a7f8c0398d36bf1e1d3869389e979cf9c46b9b0f
Bruno Ribeiro <bribeiro21@icloud.com>
2021-12-06 00:13:00 -0300

    pagina preview do post

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,55 @@
+import { fauna } from "../../../services/fauna";
+import {query as q} from 'faunadb';
+import { stripe } from "../../../services/stripe";
+
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+  createAction = false,
+) {
+  //buscar o usuario no banco de dados com o customer id = stripe customer id
+  const userRef = await fauna.query(
+    q.Select(
+      "ref",
+      q.Get(
+        q.Match(
+          q.Index('user_by_stripe_customer_id'),
+          customerId
+        )
+      )
+    )
+  )
+
+  // salvar dados da subscription do usuario no banco faunaDB
+ const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+ const subscriptionData = {
+   id: subscription.id,
+   userId: userRef,
+   status: subscription.status,
+   price_id: subscription.items.data[0].price.id,
+ }
+
+ if (createAction) {
+  await fauna.query(
+    q.Create(
+      q.Collection('subscriptions'),
+      { data: subscriptionData }
+    )
+  )
+ } else {
+  await fauna.query(
+    q.Replace(
+      q.Select(
+        "ref",
+        q.Get(
+          q.Match(
+            q.Index('subscription_by_id'),
+            subscriptionId,
+          )
+        )
+      ),
+      { data: subscriptionData }
+    )
+  )
+ }
+}
